f250fe3456ffa79d45f3ab8268c9681bfb76fdea
Aydin_Coder <80262202+Aydin-Coder@users.noreply.github.com>
2021-07-19 12:52:20 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-var zombie = []
-ready(() => {
-    every(() => random(1, 5), "seconds", () => {
-        var zombie = new Image({
-            url: "zombie.png",
-            width: 50, 
-            height: 50,
-        })
-      
-    })
-})
+setBackdropColor("#409cbc")
