950c172f387321c31ac92b52b39ed0aee4625fdb
Aydin_Coder <80262202+Aydin-Coder@users.noreply.github.com>
2021-07-19 12:42:34 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+var zombie = []
 ready(() => {
     every(() => random(1, 5), "seconds", () => {
+        var zombie = new Image({
+            url: "zombie.png",
+            width: 50, 
+            height: 50,
+        })
       
     })
 })
